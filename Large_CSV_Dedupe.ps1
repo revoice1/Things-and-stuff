254c8bd31c255165f3219984031422795f2ac49f
revoice1 <41972431+revoice1@users.noreply.github.com>
2020-05-04 14:19:51 -0700

    Update Large_CSV_Dedupe.ps1
    auto-format

--- Large_CSV_Dedupe.ps1
+++ Large_CSV_Dedupe.ps1
@@ -5,31 +5,31 @@ $OutputFile = "G:\temp\LoginHistory1588364791509_Unique.csv"
 $ShowCountEvery = 1000
 # Unit of time between progress intervals
 $DeltaUnit = "Seconds"
-$DedupeOn = "Username","Browser","Platform"
+$DedupeOn = "Username", "Browser", "Platform"
 
 # Include or exclude the data headers in the $DataForReport array from your ouput file
 $IncludeOrExclude = "include"
 # Data headers for the final report
 # included or excluded base on $IncludeOrExclude
-$DataForReport = "Username","Browser","Platform"
+$DataForReport = "Username", "Browser", "Platform"
 
 # Some pre-loop vars
-$htUnique = @{} # Empty hash table to store unique data
-$Start = get-date # Starting date/time for compare
+$htUnique = @{ } # Empty hash table to store unique data
+$Start = Get-Date # Starting date/time for compare
 $n = 0 # counter
 $LastItemTime = $null # Clearing a var for subsqeuent runs
 
-foreach($row in [System.IO.File]::ReadLines($InputFile)){
+foreach ($row in [System.IO.File]::ReadLines($InputFile)) {
     
     # Grab the header row for processing data and export later
-    if($n -eq 0){
+    if ($n -eq 0) {
         $arrHeader = $row -split ","
         
-        if($IncludeOrExclude -eq "Include"){ 
-            $csvHeader = ($arrHeader | Where-Object {$DataForReport -contains $_}) -join ","
+        if ($IncludeOrExclude -eq "Include") { 
+            $csvHeader = ($arrHeader | Where-Object { $DataForReport -contains $_ }) -join ","
         }
-        else{
-            $csvHeader = ($arrHeader | Where-Object {$DataForReport -notcontains $_}) -join ","
+        else {
+            $csvHeader = ($arrHeader | Where-Object { $DataForReport -notcontains $_ }) -join ","
         }
 
         $n++
@@ -39,57 +39,57 @@ foreach($row in [System.IO.File]::ReadLines($InputFile)){
     $n++ # Increment the counter
 
     # Show progress time to the screen at $ShowCountEvery interval
-    if($n % $ShowCountEvery -eq 0){
-        if ($LastItemTime){
-            $DeltaTime = [math]::Round($(New-TimeSpan $LastItemTime $(get-date))."Total$($DeltaUnit)",2)
+    if ($n % $ShowCountEvery -eq 0) {
+        if ($LastItemTime) {
+            $DeltaTime = [math]::Round($(New-TimeSpan $LastItemTime $(Get-Date))."Total$($DeltaUnit)", 2)
         }
-        else{
-            $DeltaTime = [math]::Round($(New-TimeSpan $Start $(get-date))."Total$($DeltaUnit)",2)
+        else {
+            $DeltaTime = [math]::Round($(New-TimeSpan $Start $(Get-Date))."Total$($DeltaUnit)", 2)
         }
 
-        $TotalTimeSpan = New-TimeSpan $Start $(get-date)
-        $SmallestTotalNonZero = (($TotalTimeSpan | select total*).psobject.properties | Where-Object {$_.MemberType -eq "NoteProperty" -and $_.value -ge 1 } | Sort-Object -Property value)[0]
-        $TotalSpanUnit = $SmallestTotalNonZero.name.Replace("Total","")
-        $TotalSpanValue = [math]::Round($SmallestTotalNonZero.value,2)
+        $TotalTimeSpan = New-TimeSpan $Start $(Get-Date)
+        $SmallestTotalNonZero = (($TotalTimeSpan | Select-Object total*).psobject.properties | Where-Object { $_.MemberType -eq "NoteProperty" -and $_.value -ge 1 } | Sort-Object -Property value)[0]
+        $TotalSpanUnit = $SmallestTotalNonZero.name.Replace("Total", "")
+        $TotalSpanValue = [math]::Round($SmallestTotalNonZero.value, 2)
         
         [pscustomobject]@{
-            number = $n
-            "Delta ($DeltaUnit)" = $DeltaTime
+            number                 = $n
+            "Delta ($DeltaUnit)"   = $DeltaTime
             "Total Time (Dynamic)" = "$TotalSpanValue $TotalSpanUnit"
         }
         $LastItemTime = Get-Date
     }
 
     # Conver the CSV data to a psObj using the header
-    $psObjRow = $row | convertfrom-csv -Header $arrHeader
+    $psObjRow = $row | ConvertFrom-Csv -Header $arrHeader
     
     #region normalization
     # Do any data normalization here
 
-    $psObjRow.Browser = switch -Wildcard ($psObjRow.Browser){
-        "*Chrome*" {"Chrome"}
-        "*Edge*" {"Edge"}
-        "*Firefox*" {"Firefox"}
-        "*Safari*" {"Safari"}
-        "*IE*" {"IE"}
-        default {$psObjRow.Browser}
+    $psObjRow.Browser = switch -Wildcard ($psObjRow.Browser) {
+        "*Chrome*" { "Chrome" }
+        "*Edge*" { "Edge" }
+        "*Firefox*" { "Firefox" }
+        "*Safari*" { "Safari" }
+        "*IE*" { "IE" }
+        default { $psObjRow.Browser }
     }
 
     #endregion normalization
 
     # Generate key and value pairs for hash table
     # Selecting item 1 of the CSV output just strips the header row
-    $key = $($psObjRow | select $DedupeOn | ConvertTo-Csv -NoTypeInformation)[1]
-    if($IncludeOrExclude -eq "Include"){ 
-        $value = $($psObjRow | select $DataForReport | ConvertTo-Csv -NoTypeInformation)[1]
+    $key = $($psObjRow | Select-Object $DedupeOn | ConvertTo-Csv -NoTypeInformation)[1]
+    if ($IncludeOrExclude -eq "Include") { 
+        $value = $($psObjRow | Select-Object $DataForReport | ConvertTo-Csv -NoTypeInformation)[1]
     }
-    else{
-        $value = $($psObjRow | select * -ExcludeProperty $DataForReport | ConvertTo-Csv -NoTypeInformation)[1]
+    else {
+        $value = $($psObjRow | Select-Object * -ExcludeProperty $DataForReport | ConvertTo-Csv -NoTypeInformation)[1]
     }
     
     # If the key doesn't alraedy exist, add it to the HT
-    if(!$htUnique[$key]){
-        $htUnique += @{$key = $value}
+    if (!$htUnique[$key]) {
+        $htUnique += @{$key = $value }
     }
 
 }
