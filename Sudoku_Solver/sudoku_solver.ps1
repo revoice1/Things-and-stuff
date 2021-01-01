df2dc6e99262fbdd65e7640e52cc5670a60d7aa7
revoice1 <41972431+revoice1@users.noreply.github.com>
2021-01-01 00:12:57 -0800

    Update sudoku_solver.ps1
    Comments mostly

--- Sudoku_Solver/sudoku_solver.ps1
+++ Sudoku_Solver/sudoku_solver.ps1
@@ -2,6 +2,7 @@ param(
     $Grid = @(8, 1, 2, 7, 5, 3, 6, 0, 9, 0, 0, 3, 6, 0, 0, 0, 0, 0, 0, 7, 0, 0, 9, 0, 2, 0, 0, 0, 5, 0, 0, 0, 7, 0, 0, 0, 0, 0, 9, 0, 4, 5, 7, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 6, 8, 0, 0, 8, 5, 0, 0, 0, 1, 0, 0, 9, 0, 0, 0, 0, 4, 0, 2)
 )
 
+# Transform grid array into actual 9x9 2-dimensional array 
 if ($Grid) {
     $Row = 0
     $SudokuGrid = New-Object "System.Collections.ArrayList"
@@ -14,11 +15,13 @@ if ($Grid) {
     $Grid = $SudokuGrid
 }
 
+# Function to find the possible values for a given box
 function Get-SudokuPossibilities {
     param(
-        $x,
-        $y
+        $x, # Column of target box
+        $y # Row of target box
     )
+    # Calculate the 3x3 box the number is in
     $XBox = switch ($x) {
         { 0..2 -contains $x } { 0..2 ; break }
         { 3..5 -contains $x } { 3..5 ; break }
@@ -29,42 +32,45 @@ function Get-SudokuPossibilities {
         { 3..5 -contains $y } { 3..5 ; break }
         { 6..8 -contains $y } { 6..8 ; break }
     }
+    # See what numbers exist in the 3x3 box
     $BoxNumbers = foreach ($RowNumber in $XBox) {
         $grid[$RowNumber][$YBox]
     }
-
+    # See what numbers exist in the column
     [array]$xNumbers = $grid[$x]
     
+    # See what numbers exist in the row
     [array]$yNumbers = foreach ($Row in $Grid) {
         $row[$y]
     }
 
-    $NotPossible = ($xNumbers + $yNumbers + $BoxNumbers)
-    $Possible = (1..9) | Where-Object { $NotPossible -notcontains $_ }
+    $NotPossible = ($xNumbers + $yNumbers + $BoxNumbers) # Numbers it can't be
+    $Possible = (1..9) | Where-Object { $NotPossible -notcontains $_ } # Possible numbers
     
     if ($Possible) {
-        return $Possible
+        return $Possible # Return array of possible numbers
     }
     else {
-        return $null
+        return $null # Return nothing if there are no possibilities
     }
 }
 
+# Function to write a 9x9 sudoku grid to the screen
 function Write-SudokuGrid {
     param(
         $Grid = $Grid
     )
-    $x = 0
+    $y = 0
     foreach ($row in $grid) {
-        if (0 -eq $x ) { "┌───────┬───────┬───────┐" }
-        if (3, 6 -contains $x ) { '├───────┼───────┼───────┤' }
+        if (0 -eq $y ) { "┌───────┬───────┬───────┐" }
+        if (3, 6 -contains $y ) { '├───────┼───────┼───────┤' }
         @(
             '│ ' + $($row[0..2] -join ' ')
             $row[3..5] -join ' '
             ($row[6..8] -join ' ') + ' │' 
         ) -join ' │ '
-        if ($x -eq 8 ) { '└───────┴───────┴───────┘' }
-        $x++
+        if ($y -eq 8 ) { '└───────┴───────┴───────┘' }
+        $y++
     }
 }
 
@@ -72,34 +78,36 @@ function Get-SudokuSolution {
     param(
         $Grid = $Grid
     )
-    $Global:StopProcessing = $false
-    $x = 0
-    foreach ($Row in $Grid) {
-        $y = 0
-        foreach ($Column in $Row) {
-            if ($Grid[$x][$y] -eq 0) {
-                $Possible = Get-SudokuPossibilities $x $y
+    $Global:Solved = $false # To stop the recursive backlash later
+    foreach ($Row in (0..8)) {
+        foreach ($Column in (0..8)) {
+            if ($Grid[$Row][$Column] -eq 0) {
+                # Get the possible numbers for a given box
+                $Possible = Get-SudokuPossibilities $Row $Column
                 foreach ($Possibility in $Possible) { 
-                    $Grid[$x][$y] = $Possibility
+                    # Set the box to a possible value
+                    $Grid[$Row][$Column] = $Possibility
+                    # Recursive call to continue attempting to solve unsolved boxes
                     Get-SudokuSolution -Grid $Grid
-                    if ($StopProcessing) {
+                    if ($Solved) {
+                        # If the puzzle has been solved don't continue the function
                         break
                     }
                     else {
-                        $Grid[$x][$y] = 0
+                        # If the puzzle hasn't been solved, and we're back, the solution didn't work
+                        # Let's backtrack by zeroing out this box and trying another possibility
+                        $Grid[$Row][$Column] = 0
                     }
                 }
-                return
+                return # There is an unsolvable box, let's backtrack
             }
-            $y++
         }
-        $x++
     }
-    $Global:StopProcessing = $True
-    return write-SudokuGrid -Grid $Grid
+    $Global:Solved = $True # If we got here, the puzzle has been solved
 }
 
-$StartTime = Get-Date
-Write-SudokuGrid -Grid $Grid
-Get-SudokuSolution -Grid $Grid
-Write-Output "Solve Time: $(New-TimeSpan $StartTime $(Get-Date))"
+Write-SudokuGrid -Grid $Grid # Write the unsolved pussle to the screen
+$StartTime = Get-Date # Store the solve start time
+Get-SudokuSolution -Grid $Grid # Find a solution
+Write-SudokuGrid -Grid $Grid # Write the solved puzzle to the screen
+Write-Output "Solve Time: $(New-TimeSpan $StartTime $(Get-Date))" # Write the solve time
